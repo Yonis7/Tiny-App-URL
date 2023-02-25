4ebb170a097fd538c4078cfc1c0ee5f8d8ccfaf5
Yonis7 <yonisaden7@gmail.com>
2023-02-25 19:13:00 +0000

    Helper funtions and exports completed along with formatting

--- express_server.js
+++ express_server.js
@@ -5,10 +5,9 @@ const app = express();
 // const cookieParser = require('cookie-parser');
 
 // We will switch to using cookie-session instead of cookie-parser because it is more secure
-const cookieSession = require('cookie-session')
+const cookieSession = require("cookie-session");
 
 const bcrypt = require("bcryptjs");
-
 
 const PORT = 8080; // default port 8080
 
@@ -25,39 +24,44 @@ const PORT = 8080; // default port 8080
 // // read session
 // const userId = req.session.user_id;
 
-
-
 app.set("view engine", "ejs");
 
 // This is used to parse the cookie header and populate req.cookies with an object keyed by the cookie names it will also have a maxAge property if the cookie contains the Expires or Max-Age attribute
-app.use(cookieSession({
-  name: 'session',
-  keys: [`key1`],
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [`key1`],
 
-  // Cookie Options
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 // app.use(cookieParser());
 // Middleware that is used to parse the body of the request sent to the server
 app.use(express.urlencoded({ extended: true }));
 
 //This function is used to generate a random string for the shortURL
 const generateRandomString = () => {
-  const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
+  const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
   for (let i = 0; i < 6; i++) {
     result += chars[Math.floor(Math.random() * chars.length)];
   }
   return result;
 };
 
-const getUserByEmail = (email) => {
-  for (const userId in users) {
-    if (users[userId].email === email) {
-      return users[userId];
-    }
-  }
-};
+//// Original function before refactoring
+
+// const getUserByEmail = (email) => {
+//   for (const userId in users) {
+//     if (users[userId].email === email) {
+//       return users[userId];
+//     }
+//   }
+// };
+
+// Refactored function required to be used from the helpers.js file
+const { getUserByEmail } = require("./helpers");
 
 const urlsForUser = (id) => {
   const filteredUrls = {};
@@ -67,20 +71,19 @@ const urlsForUser = (id) => {
     }
   }
   return filteredUrls;
-}
-
+};
 
 const users = {
   userRandomID: {
     id: "userRandomID",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur"
+    password: "purple-monkey-dinosaur",
   },
   user2RandomID: {
     id: "user2RandomID",
     email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
+    password: "dishwasher-funk",
+  },
 };
 
 const urlDatabase = {
@@ -98,8 +101,6 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -111,7 +112,6 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const userId = req.session.user_id;
 
-
   // Check if user is logged in
   if (!userId) {
     return res.status(401).send("Please log in or register");
@@ -122,21 +122,18 @@ app.get("/urls", (req, res) => {
 
   const userURL = urlsForUser(userId);
 
-
   const templateVars = {
     urls: userURL,
-    user
+    user,
   };
 
   res.render("urls_index", templateVars);
 });
 
-
 app.get("/urls/new", (req, res) => {
   // This variable is used to get the user from the users object
   const userId = req.session.user_id;
 
-  
   // This is used to check if the user exists
   if (!userId) {
     return res.status(401).send("Please log in or register");
@@ -145,26 +142,22 @@ app.get("/urls/new", (req, res) => {
     const user = users[userId];
 
     const templateVars = {
-      user
+      user,
     };
     res.render("urls_new", templateVars);
   }
   // // This variable is used to get the user from the users object
   // const user = users[req.cookies["user_id"]]
   // This variable is used to pass the user object to the urls_new.ejs file
-  
-
-  
 });
 
 app.get("/urls/:id", (req, res) => {
   const userId = req.session.user_id;
 
-
   // Check if user is logged in
   if (!userId) {
     return res.status(401).send("Please log in or register");
-  };
+  }
 
   // Check if user has access to URL
   const url = urlDatabase[req.params.id];
@@ -177,23 +170,18 @@ app.get("/urls/:id", (req, res) => {
 
   // const user = users[userId];
 
-  const templateVars = { 
-    id: req.params.id, 
+  const templateVars = {
+    id: req.params.id,
     longURL: url.longURL,
-    user: users[userId]
+    user: users[userId],
   };
   res.render("urls_show", templateVars);
 });
 
-
 app.post("/urls", (req, res) => {
-
   // This is used to get the userId from the cookies
   const userId = req.session.user_id;
 
-
-
-  
   // This is used to check if the user exists
   if (!userId) {
     return res.status(401).send("Please log in or register");
@@ -202,17 +190,14 @@ app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   //Used to add the shortURL and longURL to the urlDatabase as a key value pair where shortURL is the key and req.body.longURL is the value
   urlDatabase[shortURL] = {
-    longURL : req.body.longURL,
-    userID : userId
-
-  }
+    longURL: req.body.longURL,
+    userID: userId,
+  };
 
   // console.log(req.body); // Log the POST request body to the console
 
-    
   //Redirect the user to the new page that shows them the new short URL they created
-  res.redirect(`/urls/${shortURL}`)
-
+  res.redirect(`/urls/${shortURL}`);
 });
 
 // POST route used for deleting URLs on the server side
@@ -223,26 +208,23 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 // POST route used for updating URLs on the server side
 app.post("/urls/:id", (req, res) => {
-
   const shortURL = req.params.id;
   const newLongURL = req.body.longURL;
   urlDatabase[shortURL].longURL = newLongURL; // Update the value of the shortURL key to the new longURL value
   res.redirect("/urls");
 });
 
-
 // This route needs work to make it worki invalid urls still go through
 // Used to redirect the user to the longURL when they click on the shortURL
 app.get("/u/:id", (req, res) => {
-  
   // This variable is used to get the user from the users object
   const url = urlDatabase[req.params.id];
   // This is used to check if the url exists
   if (!url) {
     res.status(400).send("URL not found");
-    } else {
-      res.redirect(url);
-    }
+  } else {
+    res.redirect(url);
+  }
   // This is used to redirect the user to the url
 });
 
@@ -253,7 +235,6 @@ app.post("/login", (req, res) => {
 
   // This is used to check if the user exists
   if (user && bcrypt.compareSync(password, user.password)) {
-    
     // This is used to set the cookie to the user id
     req.session.user_id = user.id;
     res.redirect("/urls");
@@ -273,34 +254,31 @@ app.post("/login", (req, res) => {
 //   return null;
 // }
 
-
 // This is used to clear the cookie when the user clicks on the logout button
 app.post("/logout", (req, res) => {
-req.session = null;
-res.redirect("/login");
+  req.session = null;
+  res.redirect("/login");
 });
 
 // This route is used to render the register page
 app.get("/register", (req, res) => {
   const user = users[req.session["user_id"]];
   // This is used to check if the user is logged in if a user is logged in then it will redirect them to the urls page
-  if (user) { 
+  if (user) {
     res.redirect("/urls");
   } else {
     res.render("register");
   }
-
-  
-})
+});
 
 // This route handles the registration form data
-app.post('/register', (req,res) => {
+app.post("/register", (req, res) => {
   const id = generateRandomString();
   // This is used to get the email and password from the form
   const email = req.body.email;
   const password = req.body.password;
 
-  // this is used to check if the email or password is empty 
+  // this is used to check if the email or password is empty
   if (!email || !password) {
     res.status(400).send("Email or password is empty");
     return;
@@ -318,8 +296,8 @@ app.post('/register', (req,res) => {
   const newUser = {
     id,
     email,
-    password: hashedPassword // This is used to hash the password and store it rather than the plain text password
-  }
+    password: hashedPassword, // This is used to hash the password and store it rather than the plain text password
+  };
   // This is used to check if the email is already in the database
   users[id] = newUser;
   // This is used to set the user_id session variable
@@ -327,8 +305,7 @@ app.post('/register', (req,res) => {
 
   // This is used to redirect the user to the urls page
   res.redirect("/urls");
-
-})
+});
 
 // Rotue for the login page
 app.get("/login", (req, res) => {
@@ -337,10 +314,9 @@ app.get("/login", (req, res) => {
   if (user) {
     res.redirect("/urls");
   } else {
-  res.render("login");
+    res.render("login");
   }
-
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
