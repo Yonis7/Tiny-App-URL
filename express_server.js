580188d53414f081bb2ee7287c46003a46be7af5
Yonis7 <yonisaden7@gmail.com>
2023-02-28 13:51:24 -0500

    All bugs fixed
    Logic was implemented in multiple routes and objects were placed into new file.

--- express_server.js
+++ express_server.js
@@ -4,6 +4,12 @@ const app = express();
 // We will switch to using cookie-session instead of cookie-parser because it is more secure
 const cookieSession = require("cookie-session");
 const bcrypt = require("bcryptjs");
+const { 
+  generateRandomString, 
+  getUserByEmail, 
+  urlsForUser
+} = require("./helpers");
+const { users, urlDatabase } = require("./database");
 const PORT = 8080; // default port 8080
 
 
@@ -20,68 +26,15 @@ app.use(
   })
 );
 
-
 // Middleware that is used to parse the body of the request sent to the server
 app.use(express.urlencoded({ extended: true }));
 
-//This function is used to generate a random string for the shortURL
-const generateRandomString = () => {
-  const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
-  let result = "";
-  for (let i = 0; i < 6; i++) {
-    result += chars[Math.floor(Math.random() * chars.length)];
-  }
-  return result;
-};
-
-
-// Refactored function required to be used from the helpers.js file
-const { getUserByEmail } = require("./helpers");
-
-const urlsForUser = (id) => {
-  const filteredUrls = {};
-  for (const shortUrl in urlDatabase) {
-    if (urlDatabase[shortUrl].userID === id) {
-      filteredUrls[shortUrl] = urlDatabase[shortUrl];
-    }
-  }
-  return filteredUrls;
-};
-
-const users = {
-  userRandomID: {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
-  },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk",
-  },
-};
-
-const urlDatabase = {
-  b6UTxQ: {
-    longURL: "https://www.tsn.ca",
-    userID: "aJ48lW",
-  },
-  i3BoGr: {
-    longURL: "https://www.google.ca",
-    userID: "aJ48lW",
-  },
-};
-
 app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  if(req.session.user_id) {
+    res.redirect("/urls")
+  } else {
+    res.redirect("/login")
+  }
 });
 
 app.get("/urls", (req, res) => {
@@ -95,7 +48,7 @@ app.get("/urls", (req, res) => {
   // Get user object from users database
   const user = users[userId];
 
-  const userURL = urlsForUser(userId);
+  const userURL = urlsForUser(userId, urlDatabase);
 
   const templateVars = {
     urls: userURL,
@@ -117,8 +70,10 @@ app.get("/urls/new", (req, res) => {
     const user = users[userId];
 
     const templateVars = {
-      user,
+      user
     };
+
+    console.log(templateVars)
     res.render("urls_new", templateVars);
   }
   
@@ -132,16 +87,16 @@ app.get("/urls/:id", (req, res) => {
     return res.status(401).send("Please log in or register");
   }
 
-  // Check if user has access to URL
+  // Check if shorturl is legitimate
   const url = urlDatabase[req.params.id];
   if (!url) {
     return res.status(404).send("URL not found");
   }
+
+  // Check if user has access to URL
   if (url.userID !== userId) {
     return res.status(403).send("You do not have access to this URL");
   }
-
-  // const user = users[userId];
 
   const templateVars = {
     id: req.params.id,
@@ -167,20 +122,50 @@ app.post("/urls", (req, res) => {
     userID: userId,
   };
 
-  // console.log(req.body); // Log the POST request body to the console
-
   //Redirect the user to the new page that shows them the new short URL they created
   res.redirect(`/urls/${shortURL}`);
 });
 
 // POST route used for deleting URLs on the server side
 app.post("/urls/:id/delete", (req, res) => {
+  // Check if user is logged in
+  if (!req.session.user_id) {
+    return res.status(401).send("Please log in or register");
+  }
+
+  // Check if shorturl is legitimate
+  const url = urlDatabase[req.params.id];
+  if (!url) {
+    return res.status(404).send("URL not found");
+  }
+
+  // Check if user has access to URL
+  if (url.userID !== req.session.user_id) {
+    return res.status(403).send("You do not have access to this URL");
+  }
   const id = req.params.id;
   delete urlDatabase[id];
   res.redirect("/urls");
 });
+
 // POST route used for updating URLs on the server side
 app.post("/urls/:id", (req, res) => {
+  // Check if user is logged in
+  if (!req.session.user_id) {
+    return res.status(401).send("Please log in or register");
+  }
+
+  // Check if shorturl is legitimate
+  const url = urlDatabase[req.params.id];
+  if (!url) {
+    return res.status(404).send("URL not found");
+  }
+
+  // Check if user has access to URL
+  if (url.userID !== req.session.user_id) {
+    return res.status(403).send("You do not have access to this URL");
+  }
+
   const shortURL = req.params.id;
   const newLongURL = req.body.longURL;
   urlDatabase[shortURL].longURL = newLongURL; // Update the value of the shortURL key to the new longURL value
@@ -189,46 +174,40 @@ app.post("/urls/:id", (req, res) => {
 
 // This route needs work to make it worki invalid urls still go through
 // Used to redirect the user to the longURL when they click on the shortURL
-app.get("/u/:id", (req, res) => {
-  // This variable is used to get the user from the users object
-  const url = urlDatabase[req.params.id];
-  
+app.get("/u/:id", (req, res) => {  
   // This is used to check if the url exists
-  if (!url) {
+  if (!urlDatabase[req.params.id]) {
     res.status(400).send("URL not found");
   } else {
+    const url = urlDatabase[req.params.id].longURL;
+    // This is used to redirect the user to the url
     res.redirect(url);
   }
-  // This is used to redirect the user to the url
 });
 
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const user = getUserByEmail(email);
 
-  // This is used to check if the user exists
-  if (user && bcrypt.compareSync(password, user.password)) {
-
-    // This is used to set the cookie to the user id
-    req.session.user_id = user.id;
-    res.redirect("/urls");
-  
-  } else {
-    res.status(400).send("User not found");
+  if (!email || !password) {
+    return res.status(400).send("Email and password cannot be blank")
   }
-});
 
-// // Helper function to get user object by username
-// function getUserByUsername(username) {
-//   for (const userId in users) {
-//     const user = users[userId];
-//     if (user.username === username) {
-//       return user;
-//     }
-//   }
-//   return null;
-// }
+  const user = getUserByEmail(email, users);
+
+  if(!user) {
+    return res.status(400).send("Email not found. Please register.")
+  }
+
+  // This is used to check if the password is correct
+  if (!bcrypt.compareSync(password, user.password)) {
+    return res.status(400).send("Password incorrect")
+  }
+
+  // This is used to set the cookie to the user id
+  req.session.user_id = user.id;
+  res.redirect("/urls");
+});
 
 // This is used to clear the cookie when the user clicks on the logout button
 app.post("/logout", (req, res) => {
@@ -239,34 +218,36 @@ app.post("/logout", (req, res) => {
 // This route is used to render the register page
 app.get("/register", (req, res) => {
   const user = users[req.session["user_id"]];
+
+  const templateVars = {user}
   // This is used to check if the user is logged in if a user is logged in then it will redirect them to the urls page
   if (user) {
     res.redirect("/urls");
   } else {
-    res.render("register");
+    res.render("register", templateVars);
   }
 });
 
 // This route handles the registration form data
 app.post("/register", (req, res) => {
-  const id = generateRandomString();
   // This is used to get the email and password from the form
   const email = req.body.email;
   const password = req.body.password;
-
+  
   // this is used to check if the email or password is empty
   if (!email || !password) {
     res.status(400).send("Email or password is empty");
     return;
   }
-
+  
   // This is used to check if the email is already in the database
-  if (getUserByEmail(email)) {
-    res.status(400).send("Email is already in the database");
+  if (getUserByEmail(email, users)) {
+    res.status(400).send("Email is already in the database. Please login");
     return;
   }
-
+  
   const hashedPassword = bcrypt.hashSync(password, 10);
+  const id = generateRandomString();
 
   // This is used to create a new user object
   const newUser = {
@@ -277,7 +258,7 @@ app.post("/register", (req, res) => {
   // This is used to check if the email is already in the database
   users[id] = newUser;
   // This is used to set the user_id session variable
-  req.session.user_id = id;
+  req.session.user_id = id
 
   // This is used to redirect the user to the urls page
   res.redirect("/urls");
@@ -288,11 +269,13 @@ app.get("/login", (req, res) => {
   
   // If the user is logged in, GET /login should redirect to GET /urls
   const user = users[req.session["user_id"]];
+  const templateVars = { user }
+
   if (user) {
     res.redirect("/urls");
   
   } else {
-    res.render("login");
+    res.render("login", templateVars);
   }
 });
 
--- helpers.js
+++ helpers.js
@@ -1,4 +1,5 @@
-const getUserByEmail = function(email, users) {
+// Helper function to get user object by username
+const getUserByEmail = function (email, users) {
   for (const userId in users) {
     const user = users[userId];
     if (user.email === email) {
@@ -8,5 +9,27 @@ const getUserByEmail = function(email, users) {
   return undefined;
 };
 
+const generateRandomString = () => {
+  const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < 6; i++) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return result;
+};
 
-module.exports = { getUserByEmail };
+const urlsForUser = (id, urlDatabase) => {
+  const filteredUrls = {};
+  for (const shortUrl in urlDatabase) {
+    if (urlDatabase[shortUrl].userID === id) {
+      filteredUrls[shortUrl] = urlDatabase[shortUrl];
+    }
+  }
+  return filteredUrls;
+};
+
+module.exports = {
+  getUserByEmail,
+  generateRandomString,
+  urlsForUser,
+};
